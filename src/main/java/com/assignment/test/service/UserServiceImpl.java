37e3e6b82ab286956817a93ef491597680a3b503
Fuadhliraaa <raahmankatam6@gmail.com>
2024-10-25 11:09:20 +0700

    Update Service

--- src/main/java/com/assignment/test/service/UserServiceImpl.java
+++ src/main/java/com/assignment/test/service/UserServiceImpl.java
@@ -45,9 +45,6 @@ public class UserServiceImpl implements UserService {
   @Value("${spring.datasource.password}")
   public String PASSWORD;
   
-  @Value("${{ Postgres.DATABASE_URL }}")
-  public String DATABASE_URL;
-  
   @Transactional
   public UserRes userRegistration(UserReq req) throws RuntimeException, JsonProcessingException {
     log.info("START - USER SERVICE - USER REGISTRATION");
